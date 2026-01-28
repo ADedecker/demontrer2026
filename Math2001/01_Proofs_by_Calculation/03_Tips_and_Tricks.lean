9883920487359474fc1b86cbfceba9bbf8cafa53
Iro-IMAG <evmorfia-iro.bartzia@u-paris.fr>
2026-01-28 13:28:51 +0100

    solutions exos tp2

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -119,78 +119,123 @@ les saisir dans Lean. -/
 
 
 example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) : y = 9 := by
-  sorry
+  rw [h1] at h2
+  rw [h2]
+  ring
   done
 
 
 example {a b : ℤ} (h : a - b = 0) : a = b := by
-  sorry
+  addarith [h]
   done
 
 
 example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) : x = 14 := by
-  sorry
+  rw [h2] at h1
+  calc x = (x - 3*3) +9 := by ring
+    _    = 5+9 := by rw [h1]
+    _    = 14 := by ring
   done
 
 
 example {x y : ℚ} (h1 : y + 1 = 3) (h2 : x + 2 * y = 3) : x = -1 := by
-  sorry
+  have h3 : y = 2 := by addarith [h1]
+  calc x = (x+2*y)-2*y := by ring
+      _  =  3 -2*y := by rw [h2]
+      _  =  3-2*2 := by rw [h3]
+      _  = -1 := by ring
   done
 
 
 example {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 := by
-  sorry
+  have h3 : q = 3 := by addarith [h2]
+  rw [h3] at h1
+  calc p = (p+4*3)-12 := by ring
+      _  = 1 -12 := by rw [h1]
+      _ = -11 := by ring
   done
 
 
 example {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3) (h3 : c = 1) :
     a = 2 := by
-  sorry
-  done
+    rw [h3] at h2
+    have h4 : b = 1 := by addarith [h2]
+    rw [h3, h4] at h1
+    addarith [h1]
+    done
 
 
 example {u v : ℚ} (h1 : 4 * u + v = 3) (h2 : v = 2) : u = 1 / 4 := by
-  sorry
+  rw [h2] at h1
+  calc u = ((4*u +2)-2)/4 := by ring
+      _  = (3-2)/4 := by rw [h1]
+      _  = 1/4 := by ring
   done
 
 
 example {c : ℚ} (h1 : 4 * c + 1 = 3 * c - 2) : c = -3 := by
-  sorry
+  calc c = (4 * c + 1) - (3 * c - 2) -3 := by ring
+      _  = (3 * c - 2) - (3 * c - 2) -3 := by rw [h1]
+      _  = -3 := by ring
   done
 
 example {p : ℝ} (h1 : 5 * p - 3 = 3 * p + 1) : p = 2 := by
-  sorry
+  have h2 : 2*p = 4 := by
+    calc 2*p = (5 * p - 3)-(3 * p + 1) + 4 := by ring
+          _  = (3 * p + 1) - (3 * p + 1) + 4 := by rw [h1]
+          _  = 4 := by ring
+  calc p = (2*p)/2 := by ring
+      _  = 4/2 := by rw [h2]
+      _ = 2 := by ring
   done
 
 example {x y : ℤ} (h1 : 2 * x + y = 4) (h2 : x + y = 1) : x = 3 := by
-  sorry
+  have h3 : y = 1-x := by addarith [h2]
+  rw [h3] at h1
+  calc x = (2 * x + (1 - x)) -1 := by ring
+      _ = 4 - 1 := by rw [h1]
+      _ = 3 := by ring
   done
 
 example {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by
-  sorry
+  have h3 : b = a - 1 := by addarith [h2]
+  rw [h3] at h1
+  have h4: 3*a = 6 := by
+    calc 3*a = a  + 2*(a-1) +2 := by ring
+          _  = 4 + 2 := by rw [h1]
+          _  = 6 := by ring
+  calc a = (3*a)/3 := by ring
+      _  = 6/3 := by rw [h4]
+      _  = 2 := by ring
   done
 
 
 example {p q : ℚ} (h1 : p - 2 * q = 1) (h2 : q = -1) : p = -1 := by
-  sorry
+  rw [h2] at h1
+  addarith [h1]
   done
 
 
 example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 := by
-  sorry
+  have hx : x = 2 := by addarith [h1]
+  rw [hx] at h2
+  have hy : y*2 = 4 := by addarith [h2]
+  calc y = (y*2)/2 := by ring
+      _  = 4/2 := by rw [hy]
+      _ = 2 := by ring
   done
 
 
 example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 := by
-  sorry
+  calc u ^ 2 + 3 * u + 1 = (u+1)^2 + (u+1) -1 := by ring
+        _                = v^2 + v -1 := by rw [h1]
   done
 
-example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
-    t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 := by
-  sorry
-  done
 
 example {p q r : ℚ} (h1 : p + q + r = 0) (h2 : p * q + p * r + q * r = 2) :
     p ^ 2 + q ^ 2 + r ^ 2 = -4 := by
-  sorry
-  done
+    have H : p^2+q^2+r^2 = (p+q+r)^2 - 2*(p*q+p*r+q*r) := by ring
+    rw [H]
+    rw [h1,h2]
+    ring
+    done
