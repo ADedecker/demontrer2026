c1a4bdaf9cce32c8668d9579d8d51713a117999e
Iro-IMAG <evmorfia-iro.bartzia@u-paris.fr>
2026-02-09 15:44:42 +0100

    tatat

--- Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
+++ Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
@@ -12,7 +12,7 @@ math2001_init
 -- lemma le_antisymm {a b : ℝ} (h1 : a ≤ b) (h2 : b ≤ a) : a = b
 
 
--- comment on introduit/explique apply ?
+-- introduction apply ?
 example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
   apply ne_of_lt
   calc
@@ -21,17 +21,26 @@ example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
     _ < 1 := by numbers
   done
 
+-- exo
 example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
   apply ne_of_gt
   extra
   done
 
--- est-ce que le calc marche dans l'autre sens ?
+
 example {t : ℝ} : t ^ 2 + 2 ≠ -(1/4) := by
   have ht : t^2 + 2 ≥ 0 := by extra
   apply ne_of_gt
   calc -(1/4) < 0 := by numbers
     _         ≤ t^2 + 2 := by rel [ht]
+  done
+
+
+example {t : ℝ} : t ^ 2 + 2 ≠ -(1/4) := by
+  have ht : t^2 + 2 ≥ 0 := by extra
+  apply ne_of_gt
+  calc t^2 + 2 ≥ 0 := by rel [ht]
+    _ > -(1/4) := by numbers
   done
 
 
@@ -54,14 +63,13 @@ example {x y : ℝ} (h1 : x ^ 2 + y ^ 2 = 0) : x ^ 2 = 0 := by
   done
 
 
--- comment on fait ce truc avec le -3 ?
--- trouver un autre exemple pour se pratiquer
+
 example {x y : ℝ} (h1 : 2*x ≤ 2) (h2 : -3*x ≤ -3) : x = 1 := by
   apply le_antisymm
   · have h3 : 2*x ≤ 2*1 := by addarith [h1]
     cancel 2 at h3
-  · have h4 : -3*x ≤ -3*1 := by addarith [h2]
-    sorry
+  · have h4 : 3*1 ≤ 3*x := by addarith [h2]
+    cancel 3 at h4
   done
 
 
