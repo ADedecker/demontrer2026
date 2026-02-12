5ab4ee3d7a788afa350e17ab16cc37ad4e4e252c
Iro-IMAG <evmorfia-iro.bartzia@u-paris.fr>
2026-02-12 16:58:48 +0100

    quelques exo OU

--- Math2001/02_Proofs_with_Structure/03_Or.lean
+++ Math2001/02_Proofs_with_Structure/03_Or.lean
@@ -11,28 +11,33 @@ math2001_init
 
 -- ne_of_lt {a b : ℝ} (h : a < b) : a ≠ b
 
-
+-- ou à l'hypothese (le plus compliqué c'est ca)
+-- nommer les hypothéses generées
 example {x y : ℝ} (h : x = 1 ∨ y = -1) : x * y + x = y + 1 := by
   obtain hx | hy := h
-  calc
-    x * y + x = 1 * y + 1 := by rw [hx]
-    _ = y + 1 := by ring
-  calc
-    x * y + x = x * -1 + x := by rw [hy]
-    _ = -1 + 1 := by ring
-    _ = y + 1 := by rw [hy]
+  · calc
+      x * y + x = 1 * y + 1 := by rw [hx]
+      _ = y + 1 := by ring
+  · calc
+      x * y + x = x * -1 + x := by rw [hy]
+      _ = -1 + 1 := by ring
+      _ = y + 1 := by rw [hy]
   done
 
+
+-- preuve par cas sur un entier (ou à l'hypothese en utilisant un lemme)
 example {n : ℕ} : n ^ 2 ≠ 2 := by
   have hn := le_or_succ_le n 1
   obtain hn | hn := hn
-  apply ne_of_lt
-  calc
-    n ^ 2 ≤ 1 ^ 2 := by rel [hn]
-    _ < 2 := by numbers
-  sorry
+  · apply ne_of_lt
+    calc
+      n ^ 2 ≤ 1 ^ 2 := by rel [hn]
+      _ < 2 := by numbers
+  · sorry
   done
 
+
+-- ou à la conclusion
 example {x : ℝ} (hx : 2 * x + 1 = 5) : x = 1 ∨ x = 2 := by
   right
   calc
@@ -41,7 +46,9 @@ example {x : ℝ} (hx : 2 * x + 1 = 5) : x = 1 ∨ x = 2 := by
     _ = 2 := by numbers
   done
 
-
+-- à reecrire de facon plus lisible
+-- apply un lemme à une hypothese
+-- on peut le faire de la conclusion à la fin, c'est plus simple
 example {x : ℝ} (hx : x ^ 2 - 3 * x + 2 = 0) : x = 1 ∨ x = 2 := by
   have h1 :=
     calc
@@ -51,6 +58,10 @@ example {x : ℝ} (hx : x ^ 2 - 3 * x + 2 = 0) : x = 1 ∨ x = 2 := by
   sorry
   done
 
+-- à faire tous seuls à la fin ?
+-- mettre à la fin ?
+-- faire au tableau et la montrer en lean ?
+-- à faire la prochaine fois comme révision ?
 example {n : ℤ} : n ^ 2 ≠ 2 := by
   have hn0 := le_or_succ_le n 0
   obtain hn0 | hn0 := hn0
@@ -82,22 +93,61 @@ example {n : ℤ} : n ^ 2 ≠ 2 := by
 
 /-! # Exercises -/
 
+-- les 3 premieres preuves sont exactement pareilles (type 1)
+-- il faut les melanger avec les autres
+-- pour empecher qu'ils copient le code d'avant
 
+-- type 1
+-- on a la tendance de vouloir commencer par la conclusion ici
+-- on ne peut pas réecrire la conclusion avec addarith directement
+-- on peut faire une hypothese hh : x^2 - 16 = 0 et apres trouver x
+-- c'est quelquechose qu'on ferait en papier pour comprendre
+-- mais ca ne sert pas à faire passer la preuve formelle en lean
 example {x : ℚ} (h : x = 4 ∨ x = -4) : x ^ 2 + 1 = 17 := by
-  sorry
+  obtain h1 | h2 := h
+  · rw [h1]
+    numbers
+  · rw [h2]
+    numbers
   done
 
+-- type 1
+-- l'enjeu n'est pas le meme quand on fait une preuve formelle
+-- la partie semantique est moins important que la partie syntaxique
+-- ca veut dire que la strategie a employée est plutot de
+-- commencer par quelquepart et proceder de facon que la preuve
+-- soit acceptée par la machine logiquement / syntaxiquement
 example {x : ℝ} (h : x = 1 ∨ x = 2) : x ^ 2 - 3 * x + 2 = 0 := by
-  sorry
+  have hh : x ^ 2 - 3 * x + 2 = (x-1)*(x-2) := by ring
+  rw [hh]
+  obtain h1 | h2 := h
+  · rw [h1]
+    numbers
+  · rw [h2]
+    numbers
   done
 
+-- type 1
 example {t : ℚ} (h : t = -2 ∨ t = 3) : t ^ 2 - t - 6 = 0 := by
-  sorry
+  obtain h1 | h2 := h
+  · rw [h1]
+    numbers
+  · rw [h2]
+    numbers
   done
 
+
+-- type 1 mais avec ring car on a des variables
+-- je ne lis meme pas l'énoncé
 example {x y : ℝ} (h : x = 2 ∨ y = -2) : x * y + 2 * x = 2 * y + 4 := by
-  sorry
+  obtain h1 | h2 := h
+  · rw [h1]
+    ring
+  · rw [h2]
+    ring
   done
+
+-------------------------------------------------------
 
 example {s t : ℚ} (h : s = 3 - t) : s + t = 3 ∨ s + t = 5 := by
   sorry
