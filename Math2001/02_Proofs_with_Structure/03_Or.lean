10cf5cac42185381cea4d063799c0ab120666060
Iro-IMAG <evmorfia-iro.bartzia@u-paris.fr>
2026-02-13 17:20:31 +0100

    comments and solutions

--- Math2001/02_Proofs_with_Structure/03_Or.lean
+++ Math2001/02_Proofs_with_Structure/03_Or.lean
@@ -10,6 +10,7 @@ math2001_init
 -- eq_zero_or_eq_zero_of_mul_eq_zero {a b : ℝ}  (h : a * b = 0) : a = 0 ∨ b = 0
 
 -- ne_of_lt {a b : ℝ} (h : a < b) : a ≠ b
+-- ne_of_gt {a b : ℝ} (h : a > b) : a ≠ b
 
 -- ou à l'hypothese (le plus compliqué c'est ca)
 -- nommer les hypothéses generées
@@ -148,43 +149,149 @@ example {x y : ℝ} (h : x = 2 ∨ y = -2) : x * y + 2 * x = 2 * y + 4 := by
   done
 
 -------------------------------------------------------
+-- type 2 : OU dans la conclusion
+-- simple il faut choisir
+-- mais à quel niveau on choisit ?
 
+-- type 2 simple
 example {s t : ℚ} (h : s = 3 - t) : s + t = 3 ∨ s + t = 5 := by
-  sorry
+  left
+  addarith [h]
   done
 
+-- type 2 simple (calc un peu compliqué mais ok)
 example {a b : ℚ} (h : a + 2 * b < 0) : b < a / 2 ∨ b < - a / 2 := by
-  sorry
+  right
+  calc b = (a + 2*b)/2 - a/2 := by ring
+      _  < 0/2 - a/2 := by rel [h]
+      _ = -a/2 := by ring
   done
 
+
+-- type 2 simple
+-- probleme avec extra ici
 example {x y : ℝ} (h : y = 2 * x + 1) : x < y / 2 ∨ x > y / 2 := by
-  sorry
+  have hx : x = y/2 - 1/2 := by
+    calc x = (2*x + 1)/2 - 1/2 := by ring
+        _  = y/2 - 1/2 := by rw [h]
+  left
+  rw [hx]
+  calc y/2 - 1/2 =  y/2 + (-(1/2)) := by ring
+            _    <  y/2 + 0 := by sorry
+            _    =  y/2 := by ring
   done
 
+
+-------------------------------------------------------
+-- type 3 équation deuxieme degré
+-- application de lemme à une hypothese
+-- (1) il faut créer une hypothese de la bonne forme pour appliquer le lemme
+-- (2) on applique le lemme
+-- (3) on distingue des cas
 example {x : ℝ} (hx : x ^ 2 + 2 * x - 3 = 0) : x = -3 ∨ x = 1 := by
-  sorry
+  have h1 : x ^ 2 + 2 * x - 3 = (x + 3)*(x-1) := by ring
+  have h2 : (x + 3)*(x - 1) = 0 := by
+    rw [← h1]
+    rw [hx]
+  apply eq_zero_or_eq_zero_of_mul_eq_zero at h2
+  obtain h3 | h4 := h2
+  · left
+    addarith [h3]
+  · right
+    addarith [h4]
   done
 
-example {a b : ℝ} (hab : a ^ 2 + 2 * b ^ 2 = 3 * a * b) : a = b ∨ a = 2 * b := by
-  sorry
+-- type 3 par rapport à la strategie
+-- mais avec 2 variables
+-- (a - b)*(a - 2b) = a^2 - 2ab - ab + 2b^2
+example {a b : ℝ} (hab : a ^ 2 + 2 * b ^ 2 = 3 * a * b) :
+  a = b ∨ a = 2 * b := by
+  have h1 : (a - b)*(a - 2*b) = 0 := by
+    calc (a - b)*(a - 2*b) = a^2 - 3*a*b + 2*b^2 := by ring
+      _                    = 0 := by addarith [hab]
+    done
+  apply eq_zero_or_eq_zero_of_mul_eq_zero at h1
+  obtain h2 | h3 := h1
+  · left
+    addarith [h2]
+  · right
+    addarith [h3]
   done
 
+
+-- type 3 par rapport à la strategie
+--  on utilise cancel pour le carré
 example {t : ℝ} (ht : t ^ 3 = t ^ 2) : t = 1 ∨ t = 0 := by
-  sorry
+  have h1 : t^2 * (t-1) = 0 := by
+    calc t^2 * (t-1) = t ^ 3 - t ^ 2 := by ring
+      _              = 0 := by addarith [ht]
+  apply eq_zero_or_eq_zero_of_mul_eq_zero at h1
+  obtain h2 | h3 := h1
+  · right
+    cancel 2 at h2
+  · left
+    addarith [h3]
   done
 
+-------------------------------------------------------------------
+
+-- type 4: preuve par cas n < m OU n ≥ m+1 pour un m fixé
+
+-- le_or_succ_le {n a : ℤ} : n ≤ a ∨ a+1 ≤ n
+-- ne_of_lt {a b : ℝ} (h : a < b) : a ≠ b
+-- ne_of_gt {a b : ℝ} (h : a > b) : a ≠ b
+
+-- strategie :
+-- (1) on doit trouver le bon m
+-- (2) on utilise le lemme le_or_succ pour créer la bonne hypothese avec OU
+-- (3) faire les sous-preuves
+-- (4) dans chaque sous-preuve on doit appliquer le bon lemme
+
+-- type 4
 example {n : ℕ} : n ^ 2 ≠ 7 := by
-  sorry
+  have hn := le_or_succ_le n 2
+  obtain h1 | h2 := hn
+  · apply ne_of_lt
+    calc n^2 ≤ 2^2 := by rel [h1]
+      _      < 7 := by numbers
+    done
+  · apply ne_of_gt
+    calc n^2 ≥ 3^2 := by rel [h2]
+      _ > 7 := by numbers
   done
 
+
+-- type 4
 example {x : ℤ} : 2 * x ≠ 3 := by
-  sorry
+  have hx := le_or_succ_le x 1
+  obtain h1 | h2 := hx
+  · apply ne_of_lt
+    calc 2*x ≤ 2*1 := by rel [h1]
+      _      = 2 := by numbers
+      _      < 3 := by numbers
+    done
+  · apply ne_of_gt
+    calc 2*x ≥ 2*2 := by rel [h2]
+      _      > 3 := by numbers
+    done
   done
 
+-- type 4
 example {t : ℤ} : 5 * t ≠ 18 := by
-  sorry
+  have ht := le_or_succ_le t 3
+  obtain h1 | h2 := ht
+  · apply ne_of_lt
+    calc 5*t ≤ 5*3 := by rel [h1]
+      _      < 18 := by numbers
+    done
+  · apply ne_of_gt
+    calc 5*t ≥ 5*4 := by rel [h2]
+        _    > 18 := by numbers
+    done
   done
 
+-- type 4
+-- have to find the right number for the lemma le_or_succ_le
 example {m : ℕ} : m ^ 2 + 4 * m ≠ 46 := by
   sorry
   done
